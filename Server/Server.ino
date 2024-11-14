e6705e458409fa997138150dbb5399153a53eee4
KotNaKiske <1438027@mail.ru>
2024-11-14 22:46:05 +0300

    Update Server.ino

--- Server/Server.ino
+++ Server/Server.ino
@@ -1,103 +1,154 @@
-
 #include <WiFi.h>
 #include <ESPmDNS.h>
-#include <WiFiUdp.h>
 #include <ArduinoOTA.h>
-#include <LinkedList.h>
 #include "Button.h"
-#include <WebServer.h>
-#include <uri/UriBraces.h>
-#include <Servo.h>
+#include <ESP32Servo.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include "SPIFFS.h"
+#include <BLEDevice.h>
 
-Servo servo;
-long servoms;
-long beepms;
-bool beep;
-WebServer www(80);
-Button button[12] = {
-  Button(36, INPUT), Button(39), Button(34), Button(35), /*Button(22)*/Button(36), /*Button(23)*/Button(36),
-  Button(13, INPUT), Button(32, INPUT), Button(21), Button(0), Button(2), Button(15)
+AsyncWebServer server(80);
+WiFiClient HTTPClient;
+
+int lenbutton = 18;
+Button button[] = {
+  Button(20), Button(19), Button(8), Button(18), Button(10), Button(9), Button(46), Button(3), Button(12),
+  Button(0), Button(35), Button(2), Button(21), Button(47), Button(48), Button(45), Button(13), Button(14)
+};
+struct structureservo {
+  bool b;
+  byte pin;
+  byte off;
+  byte on;
+  Servo servo;
+  long servoms;
+  long offms;
+};
+struct structurebeep {
+  byte pin;
+  int freq;
+  byte resolution;
+  byte channel;
+  long beepms;
+  bool beep;
 };
 struct structure {
   byte pin;
   bool b;
   String s;
-  long timeron;
-  long timeroff;
+  long on;
+  long off;
   bool blazer;
 };
-structure rele[12] =  {
-  {12, false, "", -1, -1, false}, //туалет       нерабочий
-  {14, false, "&#1057;&#1055;&#1040;&#1051;&#1068;&#1053;&#1071;", -1, -1, false}, //спальня
-  {26, false, "&#1050;&#1054;&#1056;&#1048;&#1044;&#1054;&#1056;", -1, -1, false}, //коридор
-  {33, false, "&#1042;&#1040;&#1053;&#1053;&#1040;&#1071;", -1, -1, false}, //ванная
-  {25, false, "&#1058;&#1059;&#1040;&#1051;&#1045;&#1058;", -1, -1, false}, //туалет
-  {27, false, "&#1050;&#1059;&#1061;&#1053;&#1071;", -1, -1, true}, //кухня
+//ble[30];
+long bletime = -1;
+//long bleswitch = 0;
+byte blemess;
+bool blebool = false;
 
-  {4, false, "", -1, -1, false}, //склад грязи
-  {16, false, "&#1047;&#1040;&#1051;", -1, -1, false}, //зал люстра
-  {17, false, "&#1047;&#1040;&#1051;", -1, -1, false},  //ЗАЛ(СВЕТОДИОДЫ)(&#1057;&#1042;&#1045;&#1058;&#1054;&#1044;&#1048;&#1054;&#1044;&#1067;)
-  {5, false, "&#1047;&#1040;&#1051;", -1, -1, false}, //ЗАЛ(СВЕТОДИОДЫ)
-  {18, false, "&#1047;&#1040;&#1051;", -1, -1, false}, //ЗАЛ(СВЕТОДИОДЫ)
-  {19, false, "&#1047;&#1040;&#1051;", -1, -1, false} //ЗАЛ(СВЕТОДИОДЫ)
-
+structureservo servo = {true , 11, 180, 50 };
+structurebeep beep = { 1 , 2000, 12, 3};
+bool disco = false;
+long discoms;
+int lenrele = 14;
+bool au[] = {false,//кабинет 3 свето
+             false,//кабинет люстра
+             false,//каб подсветка
+             false // движение корридор
+            };
+structure rele[] = {
+  { 4, false, "Туалет", -1, -1, false },
+  { 5, false, "Ванная", -1, -1, false },
+  { 6, false, "Кухня", -1, -1, false },
+  { 7, false, "Коридор", -1, -1, false },
+  { 15, false, "Спальня", -1, -1, false },
+  { 16, false, "Кабанет", -1, -1, false },
+  { 17, false, "rele7", -1, -1, false },
+  { 42, false, "Светодиод1", -1, -1, false },
+  { 41, false, "Светодиод2", -1, -1, false },
+  { 40, false, "Светодиод3", -1, -1, false },
+  { 39, false, "Светодиод4", -1, -1, false },
+  { 38, false, "rele12", -1, -1, false },
+  { 37, false, "Зал-Люстра", -1, -1, false },
+  { 36, false, "rele14", -1, -1, false }
 };
 TaskHandle_t xbutton, xstructure;
 unsigned long previousMillisWiFi = 0;
 void setup() {
+  //neopixelWrite(RGB_BUILTIN, RGB_BRIGHTNESS, 0, 0);
   Serial.begin(115200);
-
-
-
+  BLEDevice::init("BLE");
+  if (!SPIFFS.begin(true)) {
+    Serial.println("An Error has occurred while mounting SPIFFS");
+  }
   WiFiStart();
   OTAStart();
   wwwStart();
-  for (byte i = 0; i < 12; i++) {
+
+  for (byte i = 0; i < lenrele; i++) {
     pinMode(rele[i].pin, OUTPUT);
     rele[i].b = digitalRead(rele[i].pin);
-    if (i == 6 || i == 7) {
+  }
+  //if (i == 12 || i == 13) {
+  for (byte i = 0; i < 18; i++) {
+    if (i >= 12) {
       button[i].setTapHandler(clickup);
     } else {
       button[i].setPressedHandler(clickup);
     }
-    //button[i].setBeepHandler(blazer);
     button[i].setLongHandler(clickLong);
   }
-  pinMode(23, OUTPUT);
-  pinMode(22, OUTPUT);
-
-
-  servo.attach(23);
-  servo.write(180);
-  servoms = -1;
-
-  beepms = -1;
-  beep = false;
-  ledcSetup(10, 2000, 8);
-  ledcAttachPin(22, 10);
-
+  pinMode(servo.pin, OUTPUT);
+  pinMode(beep.pin, OUTPUT);
+  servo.servo.attach(servo.pin);
+  servo.servo.write(servo.off);
+  servo.servoms = -1;
+  beep.beepms = -1;
+  beep.beep = false;
+  //ledcSetup(10, 2000, 8);       //beep
+  //ledcAttach(beep.channel, beep.freq, beep.resolution);       //beep
+  ledcAttachChannel(beep.pin, beep.freq, beep.resolution, beep.channel);
+  //ledcAttachPin(beep.pin, beep.channel);  //beep
   xTaskCreatePinnedToCore(task_button, "task_button", 10000, NULL, 1, &xbutton, 0);
   xTaskCreatePinnedToCore(task_structure, "task_structure", 20000, NULL, 1, &xstructure, 0);
+  blesend(1);
+  //neopixelWrite(RGB_BUILTIN, 0, 0, RGB_BRIGHTNESS);
+  //swrele(5,1);//кабинет
+  //swrele(12,1);//зал
 }
+
 void swrele(byte i, bool b) {
+  Serial.print(i);
+  Serial.print("|");
+  Serial.println(b);
   digitalWrite(rele[i].pin, b);
   rele[i].b = b;
-  if (!b) rele[i].timeroff = -1;
+  if (!b) rele[i].off = -1;
 }
 void blazer() {
-  ledcWrite(10, 10);
-  beep = true;
-  beepms = millis() + 300;
+  //ledcWrite(beep.channel, 10);
+  ledcWriteTone(beep.channel, beep.freq);
+  beep.beep = true;
+  beep.beepms = millis() + 300;
 }
 void loop() {
   ArduinoOTA.handle();
-  //wifiManager.process();
-  www.handleClient();
+  if (disco && (millis() - discoms >= 1000)) {
+    discoms = millis();
+    swrele(7, random(2));
+    swrele(8, random(2));
+    swrele(9, random(2));
+    swrele(10, random(2));
+  }
   if ((WiFi.status() != WL_CONNECTED) && (millis() - previousMillisWiFi >= 60000)) {
+    //neopixelWrite(RGB_BUILTIN, RGB_BRIGHTNESS, 0, 0);
     Serial.print(millis());
     Serial.println("Reconnecting to WiFi...");
     WiFi.disconnect();
     WiFi.reconnect();
     previousMillisWiFi = millis();
+  } else {
+    //neopixelWrite(RGB_BUILTIN, 0, RGB_BRIGHTNESS, 0);
   }
 }
